8a75a7d537f7ca9ce1073888a46caaf5b4ec6359
Lucas de Amorim <112043236+Luckyx128@users.noreply.github.com>
2024-08-22 18:29:27 -0300

    Doc: Explain code

--- src/main/java/com/api/Nfe/NfeApplication.java
+++ src/main/java/com/api/Nfe/NfeApplication.java
@@ -2,7 +2,12 @@ package com.api.Nfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Classe principal para iniciar a aplicação Spring boot
+ *  @author Lucas De Amorim
+ *  @version 1.0
+ *  @since 22/08/2024
+ * **/
 @SpringBootApplication
 public class 	NfeApplication {
 
--- src/main/java/com/api/Nfe/controller/MainController.java
+++ src/main/java/com/api/Nfe/controller/MainController.java
@@ -10,30 +10,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * <h1>Nfe Api</h1>
+ * <b>Note:</b> Ler atentamente essa documentação
+ * @author Lucas
+ * @version 1.0
+ * @since 22/08/2024
+ * **/
 @RestController
 @RequestMapping("/api/controller")
 public class MainController {
+
     @Autowired
     private Repository repository;
-
     private final List<Produtos> produtos = new ArrayList<>();
-
+    /**
+     * <b>Route</b>: "/" <br>
+     * <b>Method</b>: GET<br>
+     * <b>return</b> List com todos os Produtos
+     */
     @GetMapping
     public List<Produtos> listarProdutos() {
         return repository.findAll();
     }
-
+    /**
+     * <b>Route</b>: "/" <br>
+     * <b>Method</b>: Post<br>
+     * <b>return</b>  Produto cadastrado
+     * @param produto Body proveniente da requisição
+     */
     @PostMapping
     public Produtos adicionarProduto(@RequestBody @NotNull Produtos produto) {
 
         return repository.save(produto);
     }
-
+    /**
+     * <b>Route</b>: "/{id}" <br>
+     * <b>Method</b>: Get<br>
+     * <b>return</b>  Produtos com id enviado
+     * @param id identificação do produto desejado
+     */
     @GetMapping("/{id}")
     public Optional<Produtos> obterProduto(@PathVariable Long id) {
         return repository.findById(id);
     }
-
+    /**
+     * <b>Route</b>: "/{id}" <br>
+     * <b>Method</b>: Delete<br>
+     * <b>return</b>  Metodo Void
+     * @param id identificação do produto a ser deletado
+     */
     @DeleteMapping("/{id}")
     public void deletarProduto(@PathVariable Long id) {
         repository.deleteById(id);
