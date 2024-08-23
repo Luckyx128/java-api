d9eef98ef7b3f7414bb7b3f2f34e8ef771138560
Lucas Amorim <lucas.de.amorimarq@gmail.com>
2024-08-22 23:49:04 -0300

    feat: data de inclusao e quantide

--- src/main/java/com/api/Nfe/model/Produtos.java
+++ src/main/java/com/api/Nfe/model/Produtos.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 
 @Setter
 @Getter
@@ -13,7 +15,7 @@ import java.math.BigDecimal;
 public class Produtos {
 
         @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)  // Chave primária com auto-incremento
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
 
         @Column(name = "nome", nullable = false, length = 100)  // Mapeamento para a coluna "nome" no banco de dados
@@ -22,4 +24,14 @@ public class Produtos {
         @Column(name = "preco", nullable = false, precision = 10, scale = 2)  // Mapeamento para a coluna "preco" com precisão e escala
         private BigDecimal preco;
 
+        @Column(name = "data_inclusao", nullable = false, updatable = false)
+        private LocalDateTime dataInclusao;
+
+        @PrePersist
+        protected void onCreate() {
+                this.dataInclusao = LocalDateTime.now();
+        }
+
+        @Column(name = "quantidae",nullable = false)
+        private  int quantaidade;
 }
